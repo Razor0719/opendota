bf262ab0a2a19d0f28c0eb4ccf4dcea0f0f70cb2
Razor0719 <631960509@qq.com>
2018-10-09 18:06:30 +0800

    add matches params

--- apis/pro_matches/view.py
+++ apis/pro_matches/view.py
@@ -1,13 +1,20 @@
-from flask_restplus import Resource, Namespace
+from flask_restplus import Resource, Namespace, reqparse
 
 from apis.pro_matches.pro_matches import ProMatches
 
 ns = Namespace('proMatches')
 
+parser = reqparse.RequestParser()
+parser.add_argument('less_than_match_id', type=int, required=False,
+                    help='Get matches with a match ID lower than this value')
+
 
 @ns.route('/')
+@ns.param('less_than_match_id', 'Get matches with a match ID lower than this value', )
 class ProPlayersView(Resource):
     @ns.doc(description='get all ProPlayers')
     def get(self):
+        args = parser.parse_args()
+        print(args)
         proPlayers = ProMatches()
-        return proPlayers.get_all()
+        return proPlayers.get_all(params=args)
